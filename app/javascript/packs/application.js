ed9588d1be1ca77c594dd9a011467cbce8040678
korrawee <korrawee.somyos@gmail.com>
2021-12-30 22:22:14 +0700

    Add:bootstrap5

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,9 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import 'bootstrap';
+import '../bootstrap/application.js'
+import 'bootstrap-icons/font/bootstrap-icons.css'
 require('stylesheets/application.scss')
 
 
